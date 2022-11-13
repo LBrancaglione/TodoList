df3218f97449da3e58fda9568b22cd72bdf7dc81
Leonardo Brancaglione <43422065+LBrancaglione@users.noreply.github.com>
2022-11-13 14:43:44 -0300

    first development version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from "./App.Styles";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/Listitem";
+import { AddArea } from "./components/AddArea";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Work on the docs", done: true },
+    { id: 2, name: "Travel the world", done: false },
+  ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Todo List</C.Header>
+
+        <AddArea onEnter={handleAddTask}/>
+
+        {
+          /*Task List Area */
+          list.map((item, index) => (
+            <ListItem key={index} item={item} />
+          ))
+        }
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
